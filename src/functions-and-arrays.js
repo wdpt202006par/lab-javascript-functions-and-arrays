64b525aa43e96c3418f00270ac8993f1c80ce4e2
marinetomazi <m.tomazi@hotmail.fr>
2020-06-06 17:25:57 +0200

    Iteration 4 done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -48,6 +48,18 @@ sumNumbers(numbers)
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers(numbersAvg){
+  if(numbersAvg.length===0){
+    return null;
+  }else{
+    var sum=0;
+    numbersAvg.forEach(function(el){sum+=el})
+    return sum/numbersAvg.length;
+    }
+  }
+
+  averageNumbers(numbersAvg)
+
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
